8bffaf11563a773067046bd9cb7f82f01e125674
Tourist <45359262+SuYi1995@users.noreply.github.com>
2020-10-13 16:27:44 +0800

    Delete game10.bat

--- boot/game10.bat
+++
@@ -1,3 +0,0 @@
-cd ../config
-start werl +P 1024000 -smp +S 1 -name node10@192.168.1.135 -setcookie game -boot start_sasl -config ../config/elog -pa ../ebin -s main start -extra master 192.168.1.135 9001 10
-exit
