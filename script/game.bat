05a55d406c10fb94c4319d892cf64a71514d10f7
Tourist <45359262+SuYi1995@users.noreply.github.com>
2020-10-14 11:18:58 +0800

    脚本

--- script/game.bat
+++ script/game.bat
@@ -1,3 +1,3 @@
 cd ../config
-werl +P 1024000 -smp +S 1 -name node@127.0.0.1 -setcookie game -boot start_sasl -config game -pa ../ebin -s game start -extra 127.0.0.1 9001 0
+werl +P 1024000 -smp +S 1 -name node@127.0.0.1 -setcookie game -boot start_sasl -config game -pa ../ebin -s game start -extra 127.0.0.1 9001 10
 pause
