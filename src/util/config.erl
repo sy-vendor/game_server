d2b9fa7c04c09113f9ddd9bddfa0fd9a0212d39a
Tourist <45359262+SuYi1995@users.noreply.github.com>
2020-10-14 14:06:46 +0800

    配置文件

--- src/util/config.erl
+++ src/util/config.erl
@@ -14,7 +14,6 @@
     get_log_level/0,
     get_log_path/0,
     get_mysql/0,
-    get_log_mysql/0,
     is_debug/0
 ]).
 
@@ -55,34 +54,6 @@ get_mysql() ->
         _ -> "sy_game_db"
     end,
     Encode1 = case application:get_env(game, db_encode) of
-        {ok, Encode} -> Encode;
-        _ -> utf8
-    end,
-    [Host1, Port1, Roler1, Pass1, Name1, Encode1].
-
-%% @doc 获取MYSQL参数
-get_log_mysql() ->
-    Host1 = case application:get_env(game, log_db_host) of
-        {ok, Host} -> Host;
-        _ -> "localhost"
-    end,
-    Port1 = case application:get_env(game, log_db_port) of
-        {ok, Port} -> Port;
-        _ -> 3306
-    end,
-    Roler1 = case application:get_env(game, log_db_user) of
-        {ok, Roler} -> Roler;
-        _ -> "root"
-    end,
-    Pass1 = case application:get_env(game, log_db_pass) of
-        {ok, Pass} -> Pass;
-        _ -> "root"
-    end,
-    Name1 = case application:get_env(game, log_db_name) of
-        {ok, Name} -> Name;
-        _ -> "log"
-    end,
-    Encode1 = case application:get_env(game, log_db_encode) of
         {ok, Encode} -> Encode;
         _ -> utf8
     end,
